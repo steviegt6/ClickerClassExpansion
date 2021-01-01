42eb22d9ec52354be23b98e165ae5fd96e078ad8
Stevie <xxlennygamerxx@gmail.com>
2021-01-01 13:08:38 -0800

    Set up a basic clicker item

--- Content/ModdedClickerItemBase.cs
+++ Content/ModdedClickerItemBase.cs
@@ -1,9 +1,37 @@
-﻿using ClickerClass.Items;
+﻿using ClickerClass;
+using ClickerClass.Items;
 
 namespace ClickerClassExpansion.Content
 {
     public abstract class ModdedClickerItemBase : ClickerItem
     {
         public override bool Autoload(ref string name) => ClickerClassExpansion.ClickerClass.IsLoaded;
+
+        public sealed override void SetStaticDefaults()
+        {
+            ClickerSystem.RegisterClickerWeapon(this);
+
+            SafeSetStaticDefaults();
+        }
+
+        /// <summary>
+        /// This is where you set all your item's static properties, such as names/translations and arrays in ItemID.Sets. This is called after SetDefaults on the initial ModItem.
+        /// </summary>
+        public virtual void SafeSetStaticDefaults()
+        {
+        }
+
+        public sealed override void SetDefaults()
+        {
+            ClickerSystem.SetClickerWeaponDefaults(item);
+            SafeSetDefaults();
+        }
+
+        /// <summary>
+        /// This is where you set all your item's properties, such as width, damage, shootSpeed, defense, etc. For those that are familiar with tAPI, this has the same function as .json files.
+        /// </summary>
+        public virtual void SafeSetDefaults()
+        {
+        }
     }
 }
