3796f01e9fb2935ca0a5432749cb536d2609acd0
Stevie <xxlennygamerxx@gmail.com>
2021-01-01 12:42:56 -0800

    Add ClickerClass as a dependency (who needs ModCalls when you can use the direct DLL?)

--- ClickerClassExpansion.cs
+++ ClickerClassExpansion.cs
@@ -1,8 +1,23 @@
+using System;
 using Terraria.ModLoader;
 
 namespace ClickerClassExpansion
 {
-	public class ClickerClassExpansion : Mod
-	{
-	}
+    public class ClickerClassExpansion : Mod
+    {
+        /// <summary>
+        /// This version of clicker class expansion is built and made for Clicker Class v1.2.4!
+        /// </summary>
+        public static Version ClickerClassVersion { get; private set; } = new Version(1, 2, 4);
+
+        public override void Unload()
+        {
+            UnloadStaticFields();
+        }
+
+        private static void UnloadStaticFields()
+        {
+            ClickerClassVersion = null;
+        }
+    }
 }
